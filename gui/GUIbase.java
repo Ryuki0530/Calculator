ffedd1e40c5346d561d1ab582455532639d9df1d
Ryuki <ryukifujita0530@gmail.com>
2024-02-27 21:00:35 +0900

    2/27 21:00

--- gui/GUIbase.java
+++ gui/GUIbase.java
@@ -4,6 +4,13 @@ public abstract class GUIbase {
     //GUIを準備するためのメソッド
     public final void guiSet(){
         GuiInit()
-        //
+        GuiLayout()
+        GuiListener()
     }
+    //Guiの初期化を行うメソッド
+    GuiInit();
+    //Guiのレイアウトを作成するメソッド
+    GuiLayout();
+    //Guiのリスナーをセットするメソッド
+    GuiListener();
 }
